ee27d05d65ee9da9f33ae28cd8c98539b8c2ef0d
jdw3466 <jdw3466@g.rit.edu>
2018-10-04 14:48:35 -0400

    Ayyo b lemme make some them changes

---
+++ app/src/main/java/com/example/atharvashivankar/helloworld/activity_jdw3466.java
@@ -0,0 +1,13 @@
+package com.example.atharvashivankar.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_jdw3466 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jdw3466);
+    }
+}
